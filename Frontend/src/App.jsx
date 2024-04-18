9255d847b6f500532e1581527d477bf25f34a548
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-18 12:43:39 +0700

    Create Product page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,33 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import ProductList from './component/ProductList'
+import CreateProduct from './component/CreateProduct';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+        <Navbar bg="primary">
+            <Container>
+                <Link to={"/"} className='navbar-brannd text-white'>
+                    React & Laravel CRUD APP
+                </Link>
+            </Container>
+        </Navbar>
+            <Container className='mt-5'>
+                <Row>
+                    <Col md={12}>
+                        <Routes>
+                            <Route exact path='/' element={<ProductList/>} />
+                            <Route exact path='/product/create' element={<CreateProduct/>} />
+                        </Routes>
+                    </Col>
+                </Row>
+            </Container>
+    </BrowserRouter>
   )
 }
 
